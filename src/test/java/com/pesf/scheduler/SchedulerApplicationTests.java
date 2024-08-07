d5accbd631100456346e55b38722ab0bf06d9c68
Pedro Ferreira <pedroesferreira@gmail.com>
2024-08-07 23:10:55 +0100

    added files

---
+++ src/test/java/com/pesf/scheduler/SchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pesf.scheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
