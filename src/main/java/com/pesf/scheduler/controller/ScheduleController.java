b81b63848d41ac4b2f3ad0c20de9caba2f48987f
Pedro Ferreira <pedroesferreira@gmail.com>
2024-08-09 18:38:40 +0100

    gitignore upd

--- src/main/java/com/pesf/scheduler/controller/ScheduleController.java
+++ src/main/java/com/pesf/scheduler/controller/ScheduleController.java
@@ -3,6 +3,7 @@ package com.pesf.scheduler.controller;
 import com.pesf.scheduler.dto.*;
 import jakarta.validation.*;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -12,13 +13,13 @@ public class ScheduleController {
 
     @GetMapping
     @ResponseBody
-    public String getHello(@RequestParam(required = true) int associateNumber) {
-        return "Hello " + associateNumber + "!";
+    public ResponseEntity<String> getHello(@RequestParam(required = true) int associateNumber) {
+        return ResponseEntity.ok("Hello " + associateNumber + "!");
     }
 
     @PostMapping("/reserve")
-    public ScheduleResponse reserveSlot (@Valid @RequestBody ScheduleRequest scheduleRequest) {
-        return ScheduleResponse.builder()
+    public ResponseEntity<ScheduleResponse> reserveSlot (@Valid @RequestBody ScheduleRequest scheduleRequest) {
+        return ResponseEntity.ok(ScheduleResponse.builder()
                 .status(1)
                 .associateNumber(scheduleRequest.getAssociateNumber())
                 .name(scheduleRequest.getName())
@@ -27,6 +28,6 @@ public class ScheduleController {
                         .hour(scheduleRequest.getTimeslot().getHour())
                         .build())
                 .line(scheduleRequest.getLine())
-                .build();
+                .build());
     }
 }
